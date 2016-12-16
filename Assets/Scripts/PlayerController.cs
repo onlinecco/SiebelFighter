e2824d33a18ffabc9e14b159e5b1c796306eae58
onlinecco <x-joy@live.cn>
2016-12-16 12:51:00 -0600

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+[RequireComponent(typeof(ConfigurableJoint))]
+public class PlayerController : MonoBehaviour {
+
+    [SerializeField]
+    private float speed = 5f;
+    [SerializeField]
+    private float lookSensitivity = 3f;
+    [SerializeField]
+    private float thrusterForce = 1000f;
+    [Header("Spring settings:")]
+
+    [SerializeField]
+    private float jointSpring = 20f;
+    [SerializeField]
+    private float jointMaxForce = 80f;
+    private ConfigurableJoint joint;
+    private PlayerMotor motor;
+    void Start()
+    {
+        this.joint = GetComponent<ConfigurableJoint>();
+        this.motor = GetComponent<PlayerMotor>();
+        SetJointSettings(jointSpring);
+    }
+
+    void Update()
+    {
+        float _xMov = Input.GetAxisRaw("Horizontal");
+        float _zMov = Input.GetAxisRaw("Vertical");
+
+        Vector3 _moveHorizontal = transform.right * _xMov;
+        Vector3 _moveVertical = transform.forward * _zMov;
+
+        Vector3 _velocity = (_moveHorizontal + _moveVertical).normalized * speed;
+
+        motor.Move(_velocity);
+
+        float _yRot = Input.GetAxisRaw("Mouse X");
+
+        Vector3 _rotation = new Vector3(0f, _yRot, 0f) * lookSensitivity;
+
+        motor.Rotate(_rotation);
+
+        float _xRot = Input.GetAxisRaw("Mouse Y");
+
+        float _cameraRotationX = _xRot * lookSensitivity;
+
+        motor.RotateCamera(_cameraRotationX);
+
+        //Apply thruster force
+        Vector3 _thrusterForce = Vector3.zero;
+        if (Input.GetButton("Jump"))
+        {
+            _thrusterForce = Vector3.up * thrusterForce;
+            SetJointSettings(0f);
+        }else
+        {
+            SetJointSettings(jointSpring);
+        }
+
+        motor.ApplyThruster(_thrusterForce);
+    }
+
+    private void SetJointSettings(float _jointSpring)
+    {
+        joint.yDrive = new JointDrive { positionSpring = _jointSpring, maximumForce = jointMaxForce };
+    }
+}
---
+++ Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMotor : MonoBehaviour {
+
+    [SerializeField]
+    private Camera cam;
+    private Vector3 velocity = Vector3.zero;
+    private Rigidbody rb;
+    private Vector3 rotation = Vector3.zero;
+    private float cameraRotationX = 0f;
+    private Vector3 thrusterForce = Vector3.zero;
+    private float currentCameriaRotationX = 0f;
+
+    [SerializeField]
+    private float cameraRotationLimit = 85f;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    public void Move(Vector3 _velocity)
+    {
+        velocity = _velocity;
+    }
+
+    public void Rotate(Vector3 _rotation)
+    {
+        rotation = _rotation;
+    }
+
+    public void RotateCamera(float _rotation)
+    {
+        cameraRotationX = _rotation;
+    }
+    private void FixedUpdate()
+    {
+        PerformMovement();
+        PerformRotation();
+    }
+
+    void PerformRotation()
+    {
+        rb.MoveRotation(rb.rotation * Quaternion.Euler(rotation));
+        if (cam != null)
+        {
+            currentCameriaRotationX -= cameraRotationX;
+            currentCameriaRotationX = Mathf.Clamp(currentCameriaRotationX, -cameraRotationLimit, cameraRotationLimit);
+
+            cam.transform.localEulerAngles = new Vector3(currentCameriaRotationX,0f,0f);
+        }
+    }
+
+    void PerformMovement()
+    {
+        if (velocity != Vector3.zero)
+        {
+            rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+        }
+
+        if (thrusterForce != Vector3.zero)
+        {
+            rb.AddForce(thrusterForce * Time.fixedDeltaTime, ForceMode.Acceleration);
+        }
+    }
+
+    public void ApplyThruster(Vector3 _thrusterForce)
+    {
+        thrusterForce = _thrusterForce;
+    }
+}
