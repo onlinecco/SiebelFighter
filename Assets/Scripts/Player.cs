87ace5fd65c83a19cb344c7e47156d8c34a63300
qibolun <bolunqi2@illinois.edu>
2016-12-18 00:09:21 -0600

    add shooting
    add shooting

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+
+public class Player : NetworkBehaviour {
+    [SyncVar]
+    private bool _isDead = false;
+    public bool isdead
+    {
+        get { return _isDead; }
+        //only player class or class derived from player class can change it
+        protected set { _isDead = value; }
+    }
+
+
+    [SerializeField]
+    private int maxHealth = 100;
+
+    //when var changed, it will be push to client
+    [SyncVar]
+    private int currentHealth;
+
+    [SerializeField]
+    private Behaviour[] disableOnDeath;
+    private bool[] wasEnabled;
+
+
+    public void Setup()
+    {
+        wasEnabled = new bool[disableOnDeath.Length];
+        for (int i = 0; i < wasEnabled.Length; i++)
+        {
+            wasEnabled[i] = disableOnDeath[i].enabled;
+        }
+        SetDefaults();
+    }
+
+    //void Update()
+    //{
+    //    if (!isLocalPlayer)
+    //        return;
+    //    if (Input.GetKeyDown(KeyCode.K))
+    //    {
+    //        RpcTakeDamange(9999);
+    //    }
+    //}
+
+    public void SetDefaults()
+    {
+        isdead = false;
+        currentHealth = maxHealth;
+        for (int i = 0; i < disableOnDeath.Length; i++)
+        {
+            disableOnDeath[i].enabled = wasEnabled[i];
+        }
+
+        Collider _col = GetComponent<Collider>();
+        if (_col != null)
+            _col.enabled = true;
+
+    }
+    
+    //compute in any client
+    [ClientRpc]
+    public void RpcTakeDamange(int _amount)
+    {
+        if (isdead)
+            return;
+
+        currentHealth -= _amount;
+        Debug.Log(transform.name + " now has " + currentHealth + " health.");
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        isdead = true;
+
+        //DISABLE COMPONENTS
+        for (int i = 0; i < disableOnDeath.Length; i++)
+        {
+            disableOnDeath[i].enabled = false;
+        }
+        Collider _col = GetComponent<Collider>();
+        if (_col != null)
+            _col.enabled = false;
+
+
+        Debug.Log(transform.name + " is dead!.");
+
+        //CALL RESPAWN METHOD
+        //The execution of a coroutine can be paused at any point using the yield statement. The yield return value specifies when the coroutine is resumed. 
+        StartCoroutine(Respawn());
+
+    }
+
+    private IEnumerator Respawn()
+    {
+        yield return new WaitForSeconds(GameManager.instance.matchSettings.respawnTime);
+
+        SetDefaults();
+        Transform _spawnPoint = NetworkManager.singleton.GetStartPosition();
+        transform.position = _spawnPoint.position;
+        transform.rotation = _spawnPoint.rotation;
+
+        Debug.Log(transform.name + " Respawned!!");
+    }
+
+}
--- Assets/Scripts/PlayerSetup.cs
+++ Assets/Scripts/PlayerSetup.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
+[RequireComponent(typeof(Player))]
 public class PlayerSetup : NetworkBehaviour {
 
     [SerializeField]
     Behaviour[] componentsToDisable;
+
+    [SerializeField]
+    string remoteLayerName = "remotePlayer";
 
     Camera sceneCamera;
 
@@ -14,10 +18,8 @@ public class PlayerSetup : NetworkBehaviour {
     {
         if (!isLocalPlayer)
         {
-            for (int i = 0; i< componentsToDisable.Length; i++)
-            {
-                componentsToDisable[i].enabled = false;
-            }
+            DisableComponents();
+            AssignRemoteLayer();
         }
         else
         {
@@ -28,6 +30,32 @@ public class PlayerSetup : NetworkBehaviour {
             }
             
         }
+
+        GetComponent<Player>().Setup();
+    }
+
+    //method called everytime client setup locally
+    public override void OnStartClient()
+    {
+        base.OnStartClient();
+        string _netID = GetComponent<NetworkIdentity>().netId.ToString();
+        Player _player = GetComponent<Player>();
+
+        GameManager.RegisterPlayer(_netID,_player);
+    }
+
+
+    void AssignRemoteLayer()
+    {
+        gameObject.layer = LayerMask.NameToLayer(remoteLayerName);
+
+    }
+    void DisableComponents()
+    {
+        for (int i = 0; i < componentsToDisable.Length; i++)
+        {
+            componentsToDisable[i].enabled = false;
+        }
     }
 
     private void OnDisable()
@@ -36,5 +64,7 @@ public class PlayerSetup : NetworkBehaviour {
         {
             sceneCamera.gameObject.SetActive(true);
         }
+
+        GameManager.UnRegisterPlayer(transform.name);
     }
 }
