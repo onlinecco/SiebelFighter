97fcb289d372036a9914f6aa12172760082248ca
qibolun <bolunqi2@illinois.edu>
2016-12-18 14:35:00 -0600

    add graphic to player
    add graphic to player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 [RequireComponent(typeof(PlayerMotor))]
 [RequireComponent(typeof(ConfigurableJoint))]
+[RequireComponent(typeof(Animator))]
 public class PlayerController : MonoBehaviour {
 
     [SerializeField]
@@ -12,6 +13,7 @@ public class PlayerController : MonoBehaviour {
     private float lookSensitivity = 3f;
     [SerializeField]
     private float thrusterForce = 1000f;
+
     [Header("Spring settings:")]
 
     [SerializeField]
@@ -20,23 +22,27 @@ public class PlayerController : MonoBehaviour {
     private float jointMaxForce = 80f;
     private ConfigurableJoint joint;
     private PlayerMotor motor;
+    private Animator animator;
     void Start()
     {
         this.joint = GetComponent<ConfigurableJoint>();
         this.motor = GetComponent<PlayerMotor>();
+        animator = GetComponent<Animator>();
+
         SetJointSettings(jointSpring);
     }
 
     void Update()
     {
-        float _xMov = Input.GetAxisRaw("Horizontal");
-        float _zMov = Input.GetAxisRaw("Vertical");
+        float _xMov = Input.GetAxis("Horizontal");
+        float _zMov = Input.GetAxis("Vertical");
 
         Vector3 _moveHorizontal = transform.right * _xMov;
         Vector3 _moveVertical = transform.forward * _zMov;
 
-        Vector3 _velocity = (_moveHorizontal + _moveVertical).normalized * speed;
+        Vector3 _velocity = (_moveHorizontal + _moveVertical) * speed;
 
+        animator.SetFloat("ForwardVelocity", _zMov);
         motor.Move(_velocity);
 
         float _yRot = Input.GetAxisRaw("Mouse X");
